59ee94363ce23b378b95c2df2c1cc8bcd848b36e
Debanjan Datta <ddatta@vt.edu>
2018-09-29 00:21:50 -0400

    Updated code

--- src/recursive_ed.py
+++ src/recursive_ed.py
@@ -1,45 +1,72 @@
 import time
 
+
 def ED_Recursive(x, y):
-	# Empty x
-	if len(x) == 0:
-		return len(y)
-	# Empty y
-	if len(y) == 0:
-		return len(x)
+    if len(x) == 0 and len(y) == 0 :
+        return 0, ''
+    # Empty x
 
-	# check the last character
-	if x[-1] != y[-1]:
-		diff = 1
-	else:
-		diff = 0
+    if len(x) == 0:
+        return len(y), ''
+    # Empty y
+    if len(y) == 0:
+        return len(x), ''
 
-	dg = ED_Recursive(x[:-1], y[:-1]) + diff
-	up = ED_Recursive(x[:-1], y) + 1
-	lf = ED_Recursive(x, y[:-1]) + 1
-	return min(dg, up, lf)
+    # check the last character
+    if x[-1] != y[-1]:
+        diff = 1
+    else:
+        diff = 0
+
+    dg, a1 = ED_Recursive(x[:-1], y[:-1])
+    dg += diff
+    up, a2 = ED_Recursive(x[:-1], y)
+    up = up + 1
+    lf, a3 = ED_Recursive(x, y[:-1])
+    lf = lf + 1
+
+    _min = dg
+
+    if diff == 0:
+        a = a1 + 'M'
+    else:
+        a = a1 +'S'
+
+    if up < _min:
+        a = a2 + 'D'
+        _min = up
+    if lf < _min:
+        a = a3 + 'I'
+        _min = lf
+
+    return _min, a
 
 
-def run_recursive_ed(x,y):
-	t0 = time.time()
-	res = ED_Recursive(x, y)
-	t1 = time.time()
-	exec_time = t1 - t0
-	return res, exec_time
+
+def run_recursive_ed(x, y):
+    t0 = time.time()
+    res, action = ED_Recursive(x, y)
+    t1 = time.time()
+    exec_time = t1 - t0
+
+    print(list(action))
+    return res, exec_time
 
 
+def test(x=None, y=None):
+    if x is None or y is None:
+        # x = "GCGTATGCGGCTAACGC"
+        # y = "GCTATGCGGCTATACGC"
+        x = list("GCGTATGCGGC")
+        y = list("GCTATGCGGCT")
+        # x = "Shakespeare"
+        # y = "shake spear"
+    print('Input strings are :')
+    print(x)
+    print(y)
+    res, exec_time = run_recursive_ed(x, y)
+    print('Edit distance between the strings', res)
+    print('Time taken {0:.10f} seconds'.format(exec_time))
 
-def test( x = None , y = None):
-	if x is None or y is None:
-		# x = "GCTATGCCACGC"
-		# y = "GCTATGCCACGC"
-		x = "Shakespeare"
-		y = "shake spear"
-	print('Input strings are :')
-	print(x)
-	print(y)
-	res, exec_time = run_recursive_ed(x, y)
-	print('Edit distance between the strings', res)
-	print('Time taken {0:.10f} seconds'.format(exec_time))
 
-# test()
+test()
--- src/topDown_ed.py
+++ src/topDown_ed.py
@@ -1,57 +1,78 @@
 import time
 
-def ED_TopDown(x, y, memoized_val = None):
-	# Initialize the data structure to hold resulstalready computed
-	if memoized_val is None:
-		memoized_val = { }
 
-	# Handle empty strings
-	if len(x) == 0:
-		return len(y)
-	if len(y) == 0:
-		return len(x)
+def ED_TopDown(x, y, memoized_val=None):
+    # Initialize the data structure to hold resulstalready computed
+    if memoized_val is None:
+        memoized_val = {}
 
-	# if the value has been previosuly calculated - use that
-	if (len(x), len(y)) in memoized_val:
-		return memoized_val[(len(x), len(y))]
+    # Handle empty strings
+    if len(x) == 0:
+        return len(y),''
+    if len(y) == 0:
+        return len(x),''
 
-	if x[-1] == y[-1]:
-		diff = 0
-	else :
-		diff = 1
+    # if the value has been previosuly calculated - use that
+    if (len(x), len(y)) in memoized_val:
+        return memoized_val[(len(x), len(y))]
 
-	# Check the 3 cases
-	dg = ED_TopDown(x[:-1], y[:-1], memoized_val) + diff
-	up = ED_TopDown(x[:-1], y, memoized_val) + 1
-	lf = ED_TopDown(x, y[:-1], memoized_val) + 1
+    if x[-1] == y[-1]:
+        diff = 0
+    else:
+        diff = 1
 
-	# select minimum of the 3 cases
-	res = min(dg, up, lf)
-
-	# Store the calculated value
-	memoized_val[(len(x), len(y))] = res
-	# Returnthe result
-	return res
+    # Check the 3 cases
+    dg,a1 = ED_TopDown(x[:-1], y[:-1], memoized_val)
+    dg += diff
+    up,a2 = ED_TopDown(x[:-1], y, memoized_val)
+    up += 1
+    lf,a3 = ED_TopDown(x, y[:-1], memoized_val)
+    lf += 1
 
 
-def run_topdown_ed(x,y):
-	t0 = time.time()
-	res = ED_TopDown(x, y)
-	t1 = time.time()
-	exec_time = t1 - t0
-	return res , exec_time
+    _min = dg
 
-def test( x = None , y = None):
-	if x is None or y is None:
-		# x = "GCTATGCCACGC"
-		# y = "GCTATGCCACGC"
-		x = "exponential"
-		y = "polynomial"
-	print('Input strings are :')
-	print(x)
-	print(y)
-	res, exec_time = run_topdown_ed(x, y)
-	print('Edit distance between the strings', res)
-	print('Time taken {0:.10f} seconds'.format(exec_time))
+    if diff == 0:
+        a = a1 + 'M'
+    else:
+        a = a1 +'S'
+
+    if up < _min:
+        a = a2 + 'D'
+        _min = up
+    if lf < _min:
+        a = a3 + 'I'
+        _min = lf
+
+    # Store the calculated value
+    memoized_val[(len(x), len(y))] = (_min, a)
+    # Return the result
+    return _min, a
+
+
+def run_topdown_ed(x, y):
+    t0 = time.time()
+    res, action = ED_TopDown(x, y)
+    t1 = time.time()
+    exec_time = t1 - t0
+    # print(list(x))
+    # print(list(y))
+    print(list(action))
+    return res, exec_time
+
+
+def test(x=None, y=None):
+    if x is None or y is None:
+        x = "GCGTATGCGGC"
+        y = "GCTATGCGGCT"
+        # x = "exponential"
+        # y = "polynomial"
+    print('Input strings are :')
+    print(list(x))
+    print(list(y))
+    res, exec_time = run_topdown_ed(x, y)
+    print('Edit distance between the strings', res)
+    print('Time taken {0:.10f} seconds'.format(exec_time))
+
 
 test()
